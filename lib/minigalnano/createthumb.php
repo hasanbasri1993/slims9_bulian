4ab0efaf1810d0560c09cddde7e6733a1d2f0c46
Hasan Basri <hasanbasri1493@gmail.com>
2021-07-11 21:37:49 +0700

    createThumb change into res cloudinary

--- lib/minigalnano/createthumb.php
+++ lib/minigalnano/createthumb.php
@@ -32,91 +32,102 @@ $cache['exist'] = false;
 $cache['handle'] = '';
 $imagefilename = '../../' . urldecode(trim($_GET['filename']));
 
-function genContentType($imagefilename)
-{
-  if (preg_match("/.jpg$|.jpeg$/i", $imagefilename)) header('Content-type: image/jpeg');
-  if (preg_match("/.gif$/i", $imagefilename)) header('Content-type: image/gif');
-  if (preg_match("/.png$/i", $imagefilename)) header('Content-type: image/png');
-}
+//function genContentType($imagefilename)
+//{
+//
+//  if (preg_match("/.jpg$|.jpeg$/i", $imagefilename)) header('Content-type: image/jpeg');
+//  if (preg_match("/.gif$/i", $imagefilename)) header('Content-type: image/gif');
+//  if (preg_match("/.png$/i", $imagefilename)) header('Content-type: image/png');
+//}
+//
+//# Only accept JPG, PNG, GIF
+//if (!((preg_match("/.jpg$|.jpeg$/i", $imagefilename)) OR (preg_match("/.gif$/i", $imagefilename)) OR (preg_match("/.png$/i", $imagefilename)))) {
+//  header('Content-type: image/png');
+//  echo file_get_contents('wrongcontenttype.png');
+//  exit;
+//}
+//
+//// Display error image if file isn't found
+//if (!is_file($imagefilename)) {
+//  header('Content-type: image/png');
+//  echo file_get_contents('filenotfound.png');
+//  exit;
+//}
+//
+//// Display error image if file exists, but can't be opened
+//if (!is_readable($imagefilename)) {
+//  header('Content-type: image/png');
+//  echo file_get_contents('filecantbeopened.png');
+//  exit;
+//}
+//
+//$imgsize = GetImageSize($imagefilename);
+//$width = $imgsize[0];
+//$height = $imgsize[1];
+//
 
-# Only accept JPG, PNG, GIF
-if (!((preg_match("/.jpg$|.jpeg$/i", $imagefilename)) OR (preg_match("/.gif$/i", $imagefilename)) OR (preg_match("/.png$/i", $imagefilename)))) {
-  header('Content-type: image/png');
-  echo file_get_contents('wrongcontenttype.png');
-  exit;
-}
+$conf = "";
+if ((isset($_GET['width'])) AND (trim($_GET['width']) != '')) //{
+  //$res_width = $_GET['width'];
+	$conf .= "w_".$_GET['width'].",";
 
-// Display error image if file isn't found
-if (!is_file($imagefilename)) {
-  header('Content-type: image/png');
-  echo file_get_contents('filenotfound.png');
-  exit;
-}
+//} else {
+//  $res_width = $default_res_width;
+//}
 
-// Display error image if file exists, but can't be opened
-if (!is_readable($imagefilename)) {
-  header('Content-type: image/png');
-  echo file_get_contents('filecantbeopened.png');
-  exit;
-}
+if ((isset($_GET['height'])) AND (trim($_GET['height']) != ''))// {
+  //$res_height = $_GET['height'];
+	$conf .= "h_".$_GET['height'].",";
 
-$imgsize = GetImageSize($imagefilename);
-$width = $imgsize[0];
-$height = $imgsize[1];
-
-if ((isset($_GET['width'])) AND (trim($_GET['width']) != '')) {
-  $res_width = $_GET['width'];
-} else {
-  $res_width = $default_res_width;
-}
-
-if ((isset($_GET['height'])) AND (trim($_GET['height']) != '')) {
-  $res_height = $_GET['height'];
-} else {
-  $res_height = ($res_width/$width) * $height;
-}
-
-$cache['prefix'] = '_slims_img_cache_'.$res_width.'_x_'.$res_height.'_';
-$cache['file'] = $cache['folder'].$cache['prefix'].basename($imagefilename);
-
-if (file_exists($cache['file'])) {
-  $cache['exist'] = true;
-  genContentType($imagefilename);
-  echo file_get_contents($cache['file']);
-  exit;
-} else {
-  $cache['exist'] = false;
-}
-
-genContentType($imagefilename);
-
-$target = imagecreatetruecolor($res_width,$res_height);
-if (preg_match("/.jpg$|.jpeg$/i", $imagefilename)) $source = imagecreatefromjpeg($imagefilename);
-if (preg_match("/.gif$/i", $imagefilename)) $source = imagecreatefromgif($imagefilename);
-if (preg_match("/.png$/i", $imagefilename)) $source = imagecreatefrompng($imagefilename);
-
-// preserve transparency
-imagealphablending($target, false);
-imagesavealpha($target,true);
-$transparent = imagecolorallocatealpha($target, 255, 255, 255, 127);
-imagefilledrectangle($target, 0, 0, $res_width, $res_height, $transparent);
-
-imagecopyresampled($target,$source,0,0,$xoord,$yoord,$res_width,$res_height,$width,$height);
-imagedestroy($source);
-
-if ($cache['exist'] == false) {
-  if (preg_match("/.jpg$|.jpeg$/i", $imagefilename)) {
-    imagejpeg($target,null,90);
-    imagejpeg($target,$cache['file'],90);
-  }
-  if (preg_match("/.gif$/i", $imagefilename)) {
-    imagegif($target,null);
-    imagegif($target,$cache['file']);
-  }
-  if (preg_match("/.png$/i", $imagefilename)) {
-    imagepng($target,null,9);
-    imagepng($target,$cache['file'],9);
-  }
-  imagedestroy($target);
-}         
-exit;
+//} else {
+//  $res_height = ($res_width/$width) * $height;
+//}
+	
+$img_rul = "https://res.cloudinary.com/dqq8siyfu/image/upload/".$conf."q_auto:good/library/".str_replace("images/","",$_GET['filename']);
+//echo $img_rul;
+echo file_get_contents($img_rul);
+//
+//$cache['prefix'] = '_slims_img_cache_'.$res_width.'_x_'.$res_height.'_';
+//$cache['file'] = $cache['folder'].$cache['prefix'].basename($imagefilename);
+//
+//if (file_exists($cache['file'])) {
+//  $cache['exist'] = true;
+//  genContentType($imagefilename);
+//  echo file_get_contents($cache['file']);
+//  exit;
+//} else {
+//  $cache['exist'] = false;
+//}
+//
+//genContentType($imagefilename);
+//
+//$target = imagecreatetruecolor($res_width,$res_height);
+//if (preg_match("/.jpg$|.jpeg$/i", $imagefilename)) $source = imagecreatefromjpeg($imagefilename);
+//if (preg_match("/.gif$/i", $imagefilename)) $source = imagecreatefromgif($imagefilename);
+//if (preg_match("/.png$/i", $imagefilename)) $source = imagecreatefrompng($imagefilename);
+//
+//// preserve transparency
+//imagealphablending($target, false);
+//imagesavealpha($target,true);
+//$transparent = imagecolorallocatealpha($target, 255, 255, 255, 127);
+//imagefilledrectangle($target, 0, 0, $res_width, $res_height, $transparent);
+//
+//imagecopyresampled($target,$source,0,0,$xoord,$yoord,$res_width,$res_height,$width,$height);
+//imagedestroy($source);
+//
+//if ($cache['exist'] == false) {
+//  if (preg_match("/.jpg$|.jpeg$/i", $imagefilename)) {
+//    imagejpeg($target,null,90);
+//    imagejpeg($target,$cache['file'],90);
+//  }
+//  if (preg_match("/.gif$/i", $imagefilename)) {
+//    imagegif($target,null);
+//    imagegif($target,$cache['file']);
+//  }
+//  if (preg_match("/.png$/i", $imagefilename)) {
+//    imagepng($target,null,9);
+//    imagepng($target,$cache['file'],9);
+//  }
+//  imagedestroy($target);
+//}
+//exit;
