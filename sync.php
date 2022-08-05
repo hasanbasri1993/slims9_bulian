e209182b24b7284b717c7f5b2bf027f28fee324f
Hasan Basri <hasanbasri1493@gmail.com>
2022-08-05 19:17:42 +0700

    move env info define

--- sync.php
+++ sync.php
@@ -1,24 +1,13 @@
 <?php
 	
-	
 	error_reporting(E_ALL);
 	ini_set('display_errors', '1');
-	mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+	//mysqli_report(MYSQLI_REPORT_ALL ^ MYSQLI_REPORT_INDEX);
 	
-	$servername = getenv('servername');
-	$username = getenv('username');
-	$password = getenv('password');
-	$dbname = getenv('dbname');
-	
-	
-	define("DB_HOST", getenv('DB_HOST'));
-	define("DB_PORT", getenv('DB_PORT'));
-	define("DB_NAME", getenv('DB_NAME'));
-	define("DB_USERNAME", getenv('DB_USERNAME'));
-	define("DB_PASSWORD", getenv('DB_PASSWORD'));
+    include './config/sysconfig.local.inc.php';
 	
 	// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
+	$conn = new mysqli(DB_HOST_MAIN, DB_USERNAME_MAIN, DB_PASSWORD_MAIN, DB_NAME_MAIN);
 	// Check connection
 	if ($conn->connect_error) {
 		die("Connection failed: " . $conn->connect_error);
@@ -31,7 +20,31 @@
 		die("Connection failed: " . $conn_localhost->connect_error);
 	}
 	
-	$sql = "SELECT master_santri.induk AS 'member_id',CONCAT(master_santri.nama) AS 'member_name',CONCAT(master_kelas.kelas,\"\",master_rombel.rombel,\"-\",master_sekolah.sekolah) AS 'member_notes',IF (master_santri.jk=\"L\",\"1\",\"0\") AS \"gender\",CONCAT(master_santri.alamat_orangtua,\" \",master_santri.desa,\" \",kabupaten,\" \",master_santri.kodepos,\" \",master_santri.propinsi) AS 'member_address',master_santri.tanggal_lahir AS 'birth_date',master_santri.kodepos AS 'postal_code',master_santri.dulidomail AS 'member_mail_address',master_santri.dulidomail AS 'member_email',IF (foto IS NULL OR foto='','v1601997443/fotosantriaws/person-icon_v4pkh1_kysvrv.jpg',CONCAT('fotosantriaws/',master_santri.id,'/',foto)) AS 'member_image' FROM master_rombel_siswa LEFT JOIN master_rombel ON master_rombel.id=master_rombel_siswa.id_rombel LEFT JOIN master_ajaran ON master_ajaran.id=master_rombel.tahun_ajaran LEFT JOIN master_kelas ON master_kelas.id=master_rombel.id_kelas LEFT JOIN master_sekolah ON master_sekolah.id=master_kelas.id_sekolah LEFT JOIN master_santri ON master_santri.id=master_rombel_siswa.id_santri WHERE master_ajaran.STATUS='Y' AND (master_sekolah.sekolah='TMI') ORDER BY master_kelas.id ASC,master_rombel.id ASC,master_santri.nama ASC";
+	$sql = "
+SELECT
+    master_santri.induk AS 'member_id',
+    CONCAT(master_santri.nama) AS 'member_name',
+    CONCAT(master_kelas.kelas,'',master_rombel.rombel,'-',master_sekolah.sekolah) AS 'member_notes',
+    IF (master_santri.jk='L','1','0') AS 'gender',
+    CONCAT(master_santri.alamat_orangtua,' ',master_santri.desa,' ',kabupaten,' ',master_santri.kodepos,' ',master_santri.propinsi) AS 'member_address',
+    master_santri.tanggal_lahir AS 'birth_date',
+    master_santri.kodepos AS 'postal_code',
+    master_santri.dulidomail AS 'member_mail_address',
+    master_santri.dulidomail AS 'member_email',
+    IF (foto IS NULL OR foto='','v1601997443/fotosantriaws/person-icon_v4pkh1_kysvrv.jpg',CONCAT('fotosantriaws/',master_santri.id,'/',foto)) AS 'member_image'
+FROM master_rombel_siswa
+    LEFT JOIN master_rombel ON master_rombel.id=master_rombel_siswa.id_rombel
+    LEFT JOIN master_ajaran ON master_ajaran.id=master_rombel.tahun_ajaran
+    LEFT JOIN master_kelas ON master_kelas.id=master_rombel.id_kelas
+    LEFT JOIN master_sekolah ON master_sekolah.id=master_kelas.id_sekolah
+    LEFT JOIN master_santri ON master_santri.id=master_rombel_siswa.id_santri
+WHERE
+    master_ajaran.STATUS='Y' AND
+    (master_sekolah.sekolah='TMI')
+ORDER BY
+    master_kelas.id ASC,
+    master_rombel.id ASC,
+    master_santri.nama ASC";
 	$result = $conn->query($sql);
 	
 	$prepareInsert = $conn_localhost->prepare(
@@ -85,12 +98,15 @@
 			$member_mail_address = $row["member_mail_address"];
 			$member_email = $row["member_email"];
 			$member_image = $row["member_image"];
-			$mpassword = '$2y$10$3/dpe4ShTEAbIFsqf7yZWe79hNO4/LPCFz7hDo/7tNVCqmsuSB/9C';
-			$exp = '2022-05-29';
+			$mpassword = '$2y$10$CZ0AT5eeoDCa4jQGIo2kaOTy.H8Zg.oSfAXjm2Ed9YvipcREg6IjW';//1234
+			$exp = '2023-05-29';
 			$member_since_date = date("Y-m-d");
 			$member_type_id = 1;
 			
 			$prepareSelectSantri = $conn_localhost->query("SELECT member_id FROM member WHERE member_id = $member_id");
+			echo '<pre>'.$member_id;
+			var_dump($prepareSelectSantri->num_rows);
+			echo '</pre>';
 			if ($prepareSelectSantri->num_rows > 0) {
 				$prepareUpdate->bind_param("sssssssssisi", $member_name,
 					$member_notes,
