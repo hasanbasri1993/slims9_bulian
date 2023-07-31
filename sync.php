66bf8d944220a4509c230c818610c135b09c33d1
Hasan Basri <hasanbasri1493@gmail.com>
2023-07-31 20:10:19 +0700

    merge commit ce0afea598cf9a81974b3ea5d150534337b7b695

--- sync.php
+++ sync.php
@@ -1,26 +1,30 @@
 <?php
-	
-	error_reporting(E_ALL);
-	ini_set('display_errors', '1');
-	//mysqli_report(MYSQLI_REPORT_ALL ^ MYSQLI_REPORT_INDEX);
-	
-    include './config/sysconfig.local.inc.php';
-	
-	// Create connection
-	$conn = new mysqli(DB_HOST_MAIN, DB_USERNAME_MAIN, DB_PASSWORD_MAIN, DB_NAME_MAIN);
-	// Check connection
-	if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-	}
-	
-	// Create connection
-	$conn_localhost = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
-	// Check connection
-	if ($conn_localhost->connect_error) {
-		die("Connection failed: " . $conn_localhost->connect_error);
-	}
-	
-	$sql = "
+
+
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+//mysqli_report(MYSQLI_REPORT_ALL ^ MYSQLI_REPORT_INDEX);
+
+$profile = require_once './config/database.php';
+
+// Create connection
+extract($profile['nodes']["PesantrenCyber"]);
+$conn = new mysqli($host, $username, $password, $database, $port);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Create connection
+extract($profile['nodes']["SLiMS"]);
+$conn_localhost = new mysqli($host, $username, $password, $database, $port);
+// Check connection
+if ($conn_localhost->connect_error) {
+    die("Connection failed: " . $conn_localhost->connect_error);
+}
+
+$sql = "
 SELECT
     master_santri.induk AS 'member_id',
     CONCAT(master_santri.nama) AS 'member_name',
@@ -45,10 +49,10 @@ ORDER BY
     master_kelas.id ASC,
     master_rombel.id ASC,
     master_santri.nama ASC";
-	$result = $conn->query($sql);
-	
-	$prepareInsert = $conn_localhost->prepare(
-		"INSERT INTO member (
+$result = $conn->query($sql);
+
+$prepareInsert = $conn_localhost->prepare(
+    "INSERT INTO member (
 					member_id,
 					member_name,
 					member_notes,
@@ -66,9 +70,9 @@ ORDER BY
                     register_date,
                     last_update
 				) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-	
-	$prepareUpdate = $conn_localhost->prepare(
-		"UPDATE member SET
+
+$prepareUpdate = $conn_localhost->prepare(
+    "UPDATE member SET
 					member_name = ?,
 					member_notes = ?,
 					gender = ?,
@@ -82,74 +86,76 @@ ORDER BY
                     last_update = ?
 				WHERE member_id = ?
 				");
-	if ($result->num_rows > 0) {
-		$updated = 0;
-		$inserted = 0;
-		
-		while ($row = $result->fetch_assoc()) {
-			
-			$member_id = $row["member_id"];
-			$member_name = $row["member_name"];
-			$member_notes = $row["member_notes"];
-			$gender = $row["gender"];
-			$member_address = $row["member_address"];
-			$birth_date = $row["birth_date"] == "0000-00-00" ? "1997-11-11" : $row["birth_date"];
-			$postal_code = $row["postal_code"];
-			$member_mail_address = $row["member_mail_address"];
-			$member_email = $row["member_email"];
-			$member_image = $row["member_image"];
-			$mpassword = '$2y$10$CZ0AT5eeoDCa4jQGIo2kaOTy.H8Zg.oSfAXjm2Ed9YvipcREg6IjW';//1234
-			$exp = '2023-05-29';
-			$member_since_date = date("Y-m-d");
-			$member_type_id = 1;
-			
-			$prepareSelectSantri = $conn_localhost->query("SELECT member_id FROM member WHERE member_id = $member_id");
-			echo '<pre>'.$member_id;
-			var_dump($prepareSelectSantri->num_rows);
-			echo '</pre>';
-			if ($prepareSelectSantri->num_rows > 0) {
-				$prepareUpdate->bind_param("sssssssssisi", $member_name,
-					$member_notes,
-					$gender,
-					$member_address,
-					$birth_date,
-					$postal_code,
-					$member_mail_address,
-					$member_email,
-					$member_image,
-					$member_type_id,
-					$member_since_date,
-					$member_id
-				);
-				$prepareUpdate->execute();
-				$updated++;
-			} else {
-				$prepareInsert->bind_param("isssssssssssisss",
-					$member_id,
-					$member_name,
-					$member_notes,
-					$gender,
-					$member_address,
-					$birth_date,
-					$postal_code,
-					$member_mail_address,
-					$member_email,
-					$member_image,
-					$mpassword,
-					$exp,
-					$member_type_id,
-					$member_since_date,
-					$member_since_date,
-					$member_since_date
-				);
-				$prepareInsert->execute();
-				$inserted++;
-			}
-		}
-		echo "Success updated = $updated and inserted $inserted <br>";
-		echo "<a href='/'>Back To Main</a>";
-	} else {
-		echo "0 results";
-	}
-	$conn_localhost->close();
-	$conn->close();
+if ($result->num_rows > 0) {
+    $updated = 0;
+    $inserted = 0;
+
+    while ($row = $result->fetch_assoc()) {
+        $member_id = $row["member_id"];
+        $member_name = $row["member_name"];
+        $member_notes = $row["member_notes"];
+        $gender = $row["gender"];
+        $member_address = $row["member_address"];
+        $birth_date = $row["birth_date"] == "0000-00-00" ? "1997-11-11" : $row["birth_date"];
+        $postal_code = $row["postal_code"];
+        $member_mail_address = $row["member_mail_address"];
+        $member_email = $row["member_email"];
+        $member_image = $row["member_image"];
+        $mpassword = '$2y$10$CZ0AT5eeoDCa4jQGIo2kaOTy.H8Zg.oSfAXjm2Ed9YvipcREg6IjW';//1234
+        $exp = '2023-05-29';
+        $member_since_date = date("Y-m-d");
+        $member_type_id = 1;
+
+        var_dump($member_id);
+        if (!is_null($member_id)){
+            $prepareSelectSantri = $conn_localhost->query("SELECT member_id FROM member WHERE member_id = $member_id");
+            echo '<pre>' . $member_id;
+            //var_dump($prepareSelectSantri);
+            echo '</pre>';
+            if ($prepareSelectSantri->num_rows > 0) {
+                $prepareUpdate->bind_param("sssssssssisi", $member_name,
+                    $member_notes,
+                    $gender,
+                    $member_address,
+                    $birth_date,
+                    $postal_code,
+                    $member_mail_address,
+                    $member_email,
+                    $member_image,
+                    $member_type_id,
+                    $member_since_date,
+                    $member_id
+                );
+                $prepareUpdate->execute();
+                $updated++;
+            } else {
+                $prepareInsert->bind_param("isssssssssssisss",
+                    $member_id,
+                    $member_name,
+                    $member_notes,
+                    $gender,
+                    $member_address,
+                    $birth_date,
+                    $postal_code,
+                    $member_mail_address,
+                    $member_email,
+                    $member_image,
+                    $mpassword,
+                    $exp,
+                    $member_type_id,
+                    $member_since_date,
+                    $member_since_date,
+                    $member_since_date
+                );
+                $prepareInsert->execute();
+                $inserted++;
+            }
+        }
+    }
+    echo "Success updated = $updated and inserted $inserted <br>";
+    echo "<a href='/'>Back To Main</a>";
+} else {
+    echo "0 results";
+}
+$conn_localhost->close();
+$conn->close();
